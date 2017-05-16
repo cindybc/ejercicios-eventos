fce244afd254820756ec01a680dfca0404aabeab
cindy <cindialy@gmail.com>
2017-05-16 17:51:25 -0400

    Aprendiendo eventos

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+function carga(){
+  alert("Pagina CARGADA!!");
+};
+
+function clicki(){
+  var boton= document.getElementById("demo");
+  boton.addEventListener("click", function(){
+    document.getElementById("text").innerHTML = "HOLA MUNDO";
+  });
+}
+function mouse(){
+
+  var inputito = document.getElementById("cajita");
+  inputito.addEventListener("mouseover", function(){
+    alert("Estas sobre mi :P ");
+  });
+
+  inputito.addEventListener("mouseout", function(){
+    alert("No estas sobre mi")
+  });
+}
+
+mouse();
+clicki();
